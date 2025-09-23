27f0cf15557efa51eeb8fe016f0acadc4ed3dd40
Felix <fdl0005@alu.medac.es>
2025-09-23 15:17:53 +0200

    contador1

---
+++ contador.php
@@ -0,0 +1,17 @@
+<?php
+
+
+for ($i = 0; $i < 4; $i++) {
+    for ($j = 0; $j < 4; $j++) {
+        if ($j == 1) {
+            echo "X ";
+        }
+        else {
+            echo "O ";
+        }
+    }
+    echo "\n";
+}
+
+
+?>
