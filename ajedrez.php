3870c300b93d4baa60ac993616c60b88b215d029
Felix <fdl0005@alu.medac.es>
2025-09-23 17:49:39 +0200

    ajedrez

--- ajedrez.php
+++ ajedrez.php
@@ -4,10 +4,10 @@ $n = 9;
 for ($i = 0; $i < $n; $i++) {
     for ($j = 0; $j < $n; $j++) {
         if (($i + $j) % 2 == 0) {
-            echo "B ";
+            echo "⬜ ";
         }
         else {
-            echo "N ";
+            echo "⬛ ";
         }
     } 
 
