0d040faf332643057f53e58aec002764e83ed00a
Felix <fdl0005@alu.medac.es>
2025-09-23 16:08:44 +0200

    validar contraseña

---
+++ validarcontraseña.php
@@ -0,0 +1,27 @@
+<?php
+function validarPass( string $pass): bool 
+{
+    if (strlen($pass) < 8) return false;
+    $tieneMayus = (bool) preg_match('/[A-Z]/', $pass);
+    $tieneMinus = (bool) preg_match('/[a-z]/', $pass);
+    $tieneDig = (bool) preg_match('/\d/', $pass);
+    return $tieneMayus && $tieneMinus && $tieneDig;
+
+}
+
+$p = "Hola1dryghdi";
+if (validarPass($p)) {
+    echo "Conraseña válida";
+}else {
+    echo "Contraseña no válida";
+}
+
+
+
+
+
+
+
+
+
+?>
