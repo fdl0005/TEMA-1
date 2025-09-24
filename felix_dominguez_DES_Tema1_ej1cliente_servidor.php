2c463a32f861092072869f1ac370fd024a47453e
Felix <fdl0005@alu.medac.es>
2025-09-24 17:14:51 +0200

    cliente-servidor

---
+++ felix_dominguez_DES_Tema1_ej1cliente_servidor.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+    <html lang="es">    
+    <head>
+
+        <meta charset="UTF-8">
+        <tittle>Cliente-Servidor<tittle>
+            
+    </head>
+
+    <body>
+        <h1>
+            <?php
+    
+                $array = ["Félix", "Domínguez", "fdl0005@alu.medac.es"];
+                echo "<h1>" . $array[0] . "</h1>" . " ";
+                echo "<h1>" . $array[1] . "</h1>" . " ";
+                echo "<h1>" . $array[2] . "</h1>";
+                
+            ?>
+        </h1>
+
+    </body>
+</html>    
