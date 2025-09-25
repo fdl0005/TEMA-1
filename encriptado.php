181f6470264bbc6066a07aea01bb56ddaaf3e698
Felix <fdl0005@alu.medac.es>
2025-09-25 16:33:59 +0200

    contraseña encriptada

--- encriptado.php
+++ encriptado.php
@@ -1,32 +1,61 @@
 <?php
 function codificar($contraseña) {
-    
-    $letras = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "ñ", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-    $letrasinv = ["z", "y", "x", "w", "v", "u", "t", "s", "r", "q", "p", "o", "ñ", "n", "m", "l", "k", "j", "i", "h", "g", "f", "e", "d", "c", "b", "a"];
-    
-    strlen($contraseña);
+    $letras = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+    $letrasinv = ["z","y","x","w","v","u","t","s","r","q","p","o","n","m","l","k","j","i","h","g","f","e","d","c","b","a"];
 
-    for ($i = 0; $i < count($letras); $i++) {
-        for ($j =0; $j < count($letrasinv); $j++) {
-            if ($letras[$i] == $letrasinv[$j]) {
-                $contraseña = $letrasinv[$j];
-                echo $contraseña;
+    $resultado = "";
+
+    for ($i = 0; $i < strlen($contraseña); $i++) {
+        $caracter = $contraseña[$i];
+        $encontrado = false;
+
+        for ($j = 0; $j < count($letras); $j++) {
+            if ($caracter == $letras[$j]) {
+                $resultado .= $letrasinv[$j];
+                $encontrado = true;
+                break;
             }
+        }
+
+        if (!$encontrado) {
+
+            $resultado .= $caracter;
         }
     }
 
-
+    return $resultado;
 }
 
 function descodificar($contraseña) {
+    $letras = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+    $letrasinv = ["z","y","x","w","v","u","t","s","r","q","p","o","n","m","l","k","j","i","h","g","f","e","d","c","b","a"];
 
+    $resultado = "";
 
+    for ($i = 0; $i < strlen($contraseña); $i++) {
+        $caracter = $contraseña[$i];
+        $encontrado = false;
 
+        for ($j = 0; $j < count($letrasinv); $j++) {
+            if ($caracter == $letrasinv[$j]) {
+                $resultado .= $letras[$j];
+                $encontrado = true;
+                break;
+            }
+        }
+
+        if (!$encontrado) {
+            $resultado .= $caracter;
+        }
+    }
+
+    return $resultado;
 }
 
-$contraseña = "contrasena";
-codificar($contraseña);
+$contraseña = "contraseña";
+$codificada = codificar($contraseña);
+$descodificada = descodificar($codificada);
 
-
-
+echo "Contraseña codificada: " . $codificada . "\n";
+echo "Contraseña decodificada: " . $descodificada . "\n";
 ?>
